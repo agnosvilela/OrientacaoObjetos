12f962342d1ad125e0f9def6ca71e0564d156a0a
agnos.nl@gmail.com <agnos.nl@gmail.com>
2023-03-20 14:47:07 -0300

    adicionando classes e objetos

---
+++ 05-AprendendoMaisOrientacaoObjetos/Conta.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Contas
+{
+    public class Conta
+    {
+        public string Numero;
+        public DateTime DataAbertura;
+        public Decimal Saldo;
+
+    }
+}
--- 05-AprendendoMaisOrientacaoObjetos/Program.cs
+++ 05-AprendendoMaisOrientacaoObjetos/Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
-
-namespace AprendendoMaisOrientacaoObjetos
+using Contas;
+namespace _05_AprendendoMaisOrientacaoObjetos
 {
     class Program
     {
         public static void Main(string[] args)
         {
             // classes e objetos.
-            
-            Console.WriteLine("Hello, World!");
+            // como instanciar a classe na classe program? veja:
+
+            var conta1 = new Conta();
+            conta1.Numero = "001";
+            conta1.DataAbertura = DateTime.Today;
+            conta1.Saldo = 0;
+
+            Console.WriteLine(conta1.Numero);
+            Console.WriteLine(conta1.DataAbertura);
+            Console.WriteLine(conta1.Saldo);
         }
     }
 
