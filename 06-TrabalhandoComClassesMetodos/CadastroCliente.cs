30350541b0527db413715451cdc98b430f83fcc0
agnos.nl@gmail.com <agnos.nl@gmail.com>
2023-03-26 20:20:54 -0300

    Aprimorando os conhecimentos em namespaces

---
+++ 06-TrabalhandoComClassesMetodos/CadastroCliente.cs
@@ -0,0 +1,12 @@
+
+namespace Cadastro
+{
+    class Clientes
+    {
+        public string Nome;
+        public string Email;
+        public string CPF;
+        public DateTime DataNascimento;
+        public string Profissao;
+    }
+}
---
+++ 06-TrabalhandoComClassesMetodos/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Cadastro;
+namespace TrabalhandoComClassesMetodos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cliente1 = new Clientes();
+            cliente1.Nome = "Agnos Vilela";
+            cliente1.Profissao = "Professor";
+            cliente1.CPF = "000.002.003.00";
+            cliente1.Email = "agnos.nl@gmail.com";
+            cliente1.DataNascimento = new DateTime(2023, 04, 22);
+
+            Console.WriteLine(cliente1.Nome);
+        }
+    }
+
+}
