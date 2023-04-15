e8ae04fd042ca1163d4687f55fc81e4705add477
agnos.nl@gmail.com <agnos.nl@gmail.com>
2023-04-15 19:55:39 -0300

    introdução aos metodos assessores

---
+++ 08-MetodosAssessores/Funcionario.cs
@@ -0,0 +1,15 @@
+namespace RH
+{
+    public class Funcionario
+    {
+        public string name;
+        public string CPF;
+
+        public Funcionario()
+        {
+            Console.WriteLine("Metodo construtor inicializado");
+        }
+    }
+
+
+}
---
+++ 08-MetodosAssessores/Program.cs
@@ -0,0 +1,10 @@
+﻿using RH;
+
+
+var funcionario1 = new Funcionario();
+funcionario1.name = "Agnos Vilela";
+funcionario1.CPF = "055.066.077.00";
+
+Console.WriteLine(funcionario1.name);
+Console.WriteLine(funcionario1.CPF);
+ 
