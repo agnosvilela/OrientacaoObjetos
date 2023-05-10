43a839fb43d2c40df26fff9fd306849e7c30ddf4
agnos.nl@gmail.com <agnos.nl@gmail.com>
2023-05-09 21:47:41 -0300

    trabalhando com herança

---
+++ 09-Heranca/Personagens/Arqueiro.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+namespace Personagens
+{
+    public class Arqueiro : Personagem
+    {
+        public Arqueiro(string nome, int vida) : base(nome, vida)
+        {
+
+        }
+
+    }
+
+}
---
+++ 09-Heranca/Personagens/Cavaleiro.cs
@@ -0,0 +1,13 @@
+namespace Personagens
+{
+   public class Cavaleiro : Personagem
+    {
+
+        public Cavaleiro(string nome, int vida) : base(nome, vida)
+        {
+           
+        }
+
+    }
+
+}
